073dd210068944c7bf9a5b4c60b09cace8e2ef02
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-11-19 15:46:02 +0100

    chore: updated parameter name

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,8 @@
 import { postExists } from './utils';
 import inject from './injector';
 
-export default function render(val: 'home' | 'post' | '404', postid?: string) {
-  switch (val) {
+export default function render(view: 'home' | 'post' | '404', postid?: string) {
+  switch (view) {
     case 'home':
       inject('home');
       break;
