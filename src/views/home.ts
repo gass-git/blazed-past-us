7da3f34bcda0449b45e08944559b2bfcfad16bac
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-11-20 17:34:24 +0100

    feat: posts are now wrapped within a card with a subtle hover effect

--- src/views/home.ts
+++ src/views/home.ts
@@ -3,14 +3,20 @@ import { postsMetaData } from '../main';
 export default function home(): string {
   let html = '';
 
-  html += `<h1>gass-git</h1>`;
+  html += `
+    <div class="padding-20">
+      <h1>gass-git</h1>
+    </div>
+  `;
 
   postsMetaData.forEach((post) => {
     html += `
       <a href="${post.id}">
-        <h2>${post.title}</h2>
+        <div class="post-card">
+          <h2>${post.title}</h2>
+          <h3>${post.brief}</h3>
+        </div>
       </a>
-        <h3>${post.brief}</h3>
     `;
   });
 
