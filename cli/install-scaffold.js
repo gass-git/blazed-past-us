4156c0d3a01f5d5b1c437314b752234c4f17fa42
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-12-05 16:51:42 +0100

    feat: adds the scaffolding script

---
+++ cli/install-scaffold.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const path = {
+  template: path.resolve(import.meta.dirname, '../template'),
+  target: process.cwd(),
+};
+
+function copyRecursive(src, destination) {
+  // does source path exist ?
+  if (fs.existsSync(src)) {
+    // does source path point to a directory ?
+    if (fs.statSync(src).isDirectory()) {
+      // if destination dir doesn't exist, create it.
+      if (!fs.existsSync(destination)) fs.mkdirSync(destination);
+
+      // resursively copy all files from source to destination.
+      fs.readdirSync(src).forEach((file) => {
+        copyRecursive(path.join(src, file), path.join(destination, file));
+      });
+    } else {
+      // if file in src path, copy file to destination.
+      fs.copyFileSync(src, destination);
+    }
+  }
+}
+
+copyRecursive(path.template, path.target);
+
+console.log(chalk.yellow('Scafollding set ✅'));
