6961abd330c43356931a4f0b71a7865a6c3d65c4
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-01-20 18:58:56 +0100

    refactor: minor main.js change

--- src/template/src/main.js
+++ src/template/src/main.js
@@ -1,7 +1,6 @@
 import config from './config.json';
-import { getPostsMetaData } from 'blazed-past-us';
+import { getPostsMetaData, showCosmicSpeed } from 'blazed-past-us';
 import router from './router';
-import { showCosmicSpeed } from 'blazed-past-us';
 
 const root = document.getElementById(config.root_id);
 const speedElement = document.getElementById(config.speed_element_id);
