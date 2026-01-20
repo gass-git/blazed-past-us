1eeaf3aef25d39f386b762ee0ae0b868ee1eeadc
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-01-20 18:23:03 +0100

    refactor: install-scaffold.ts cleanup

--- src/cli/install-scaffold.ts
+++ src/cli/install-scaffold.ts
@@ -9,21 +9,32 @@ const dir = {
 };
 
 copyRecursive(dir.template, dir.target);
+console.log(chalk.green('scafolld installed ✅'));
 
 function copyRecursive(src: string, destination: string): void {
-  if (fs.existsSync(src)) {
-    if (fs.statSync(src).isDirectory()) {
-      if (!fs.existsSync(destination)) {
-        fs.mkdirSync(destination);
-      }
+  const srcPathExists = fs.existsSync(src);
 
-      fs.readdirSync(src).forEach((file) => {
-        copyRecursive(path.join(src, file), path.join(destination, file));
-      });
-    } else {
-      fs.copyFileSync(src, destination);
+  if (!srcPathExists) {
+    return;
+  }
+
+  if (fs.statSync(src).isDirectory()) {
+    /**
+     * Make a folder at the destination path with the same name
+     * as the one in src.
+     */
+    if (!fs.existsSync(destination)) {
+      fs.mkdirSync(destination);
     }
+
+    // Copy the content of the src folder to the destination.
+    fs.readdirSync(src).forEach((item) => {
+      const itemSrcPath = path.join(src, item);
+      const itemDestinationPath = path.join(destination, item);
+
+      copyRecursive(itemSrcPath, itemDestinationPath);
+    });
+  } else {
+    fs.copyFileSync(src, destination);
   }
 }
-
-console.log(chalk.yellow('scafollding set ✅'));
