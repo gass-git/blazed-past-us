ee834785a8e340104791a2aab17091bf9d070be0
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-01-29 21:55:54 +0100

    feat: the code snippets are now customizable by the consumer

--- src/server/parse-markdown.ts
+++ src/server/parse-markdown.ts
@@ -1,9 +1,11 @@
 import { readFile } from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
 import { remark } from 'remark';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeStringify from 'rehype-stringify';
+import path from 'node:path';
 
 /**
  * Converts a Markdown file to HTML with syntax highlighting.
@@ -11,13 +13,19 @@ import rehypeStringify from 'rehype-stringify';
  * Uses remark and rehype to parse Markdown, transform it to HTML,
  * and apply pretty code highlighting.
  */
-async function parseMarkdown(path: string): Promise<string> {
-  const markdown = await readFile(path);
+async function parseMarkdown(_path: string): Promise<string> {
+  const root = process.cwd();
+  const markdown = await readFile(_path);
 
   const result = await remark()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypePrettyCode, { theme: 'dark-plus', keepBackground: false })
+    .use(rehypePrettyCode, {
+      theme: JSON.parse(
+        readFileSync(path.join(root, 'src/moonlight-li.json'), 'utf-8')
+      ),
+      keepBackground: false,
+    })
     .use(rehypeStringify)
     .process(markdown);
 
