81f2737ba2b0d965d8d05bd66cf236ebb96b8220
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-01-31 19:45:54 +0100

    feat: the title of the blog is set automatically from the value in package.json

--- src/template/src/main.js
+++ src/template/src/main.js
@@ -1,6 +1,12 @@
 import config from './config.json';
+import pkg from '../package.json';
 import { getPostsMetaData, showCosmicSpeed } from 'blazed-past-us';
 import router from './router';
+
+document.getElementById(config.title_id).innerText = pkg.name.replaceAll(
+  '-',
+  ' '
+);
 
 const root = document.getElementById(config.root_id);
 const speedElement = document.getElementById(config.speed_element_id);
