76fb2f292a848ee39a6931026d8176b66ff6ede5
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-02-10 14:00:15 +0100

    feat: logo rotation effect on page load

--- src/template/src/main.js
+++ src/template/src/main.js
@@ -3,10 +3,7 @@ import pkg from '../package.json';
 import { getPostsMetaData, showCosmicSpeed } from 'blazed-past-us';
 import router from './router';
 
-document.getElementById(config.title_id).innerText = pkg.name.replaceAll(
-  '-',
-  ' '
-);
+document.getElementById(config.title_id).innerText = pkg.name.replaceAll('-', ' ');
 
 const root = document.getElementById(config.root_id);
 const speedElement = document.getElementById(config.speed_element_id);
@@ -14,5 +11,9 @@ const postsMetaData = await getPostsMetaData(config);
 
 showCosmicSpeed(speedElement);
 router(root, postsMetaData);
+
+// logo rotation effect
+const wrapper = document.querySelector('.logo .wrapper');
+wrapper.classList.add('rotate');
 
 export { root, postsMetaData };
